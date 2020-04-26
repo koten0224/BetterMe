e5287ab315efe7b5bada3fea8e804b8af115bb08
koten0224 <koten0224@gmail.com>
2020-04-26 17:47:54 +0800

    修改devise原生controller的行為，嘗試增加使用者名稱欄位並使其通過permit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    puts "----------hello! session check:----------"
+    session.keys.each{|k| puts "#{k}: #{session[k]}"}
+    puts "----------session check done.------------"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_072408) do
+ActiveRecord::Schema.define(version: 2020_04_26_082333) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_072408) do
     t.string "fb_token"
     t.string "google_uid"
     t.string "google_token"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

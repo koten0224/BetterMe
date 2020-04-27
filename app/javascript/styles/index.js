82aeac5a0ef8f04e1ecfb64015468eca91f31221
koten0224 <koten0224@gmail.com>
2020-04-27 12:45:49 +0800

    前端套件整理，現在可以正確顯示bootstrap樣式了

---
+++ app/javascript/styles/index.js
@@ -0,0 +1,5 @@
+import "@fortawesome/fontawesome-free/css/all.css"
+import "bootstrap/dist/css/bootstrap.css"
+import "jquery/dist/jquery.js"
+import "popper.js/dist/popper.js"
+import "bootstrap/dist/js/bootstrap.js"
